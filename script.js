70555b1100f053b05a777cbd1f9e0d398f652a47
AbdulMoeez060 <abdulmoeez0812@gmail.com>
2022-09-27 10:18:39 +0500

    add players and events

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
 
+const gamePlay = (()=>{
+    var playerOne;
+    var playerTwo;
+    var huButton = document.querySelector('.human');
+    huButton.addEventListener('click',vsHuman);
+    function vsHuman(){
+        playerOne = Player('Player 1','x',true);
+        playerTwo = Player('Player 2','o',false);
+        var buttons = document.querySelector('.buttons');
+        buttons.style.display = 'none'
+        var board = document.querySelector('.board');
+        board.style.display = 'grid'
+
+    }
+    function getCurrentPlayer(){
+        return playerOne.turn? playerOne : playerTwo;
+    }
+
+    
+    return {getCurrentPlayer};
+
+})();
+
 
 function Player(name,symbol,turn){
     return {name,symbol,turn,won:0};
 }
 
 const boardController = (()=>{
-var board = document.querySelector('.board');
+    var board = document.querySelector('.board');
     var cells = document.querySelectorAll('.cell');
 
     cells.forEach(cell=> cell.addEventListener('click',handleClick,{once:true}))
     
 
     function handleClick(e){
-        console.log(e.target)
+        console.log(gamePlay.getCurrentPlayer());
     }
-    
-})()
-
-const playerController = (()=>{
-
 })()
 
 
-const gamePlay = (()=>{
-    
-    //board.addEventListener('click',mark);,{once:true}to only fire event listener once
 
-})();
